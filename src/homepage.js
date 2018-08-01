5f5601596516686afd621b8398bf21fed568f38a
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-01 11:48:33 +0200

    added a method to generate homepage contents

---
+++ src/homepage.js
@@ -0,0 +1,22 @@
+export default function generateHomepage() {
+  // create wrapper div
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  // create heading
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Welcome to Nonexistent Restaurant';
+  // create image
+  const img = document.createElement('img');
+  img.setAttribute('src', './images/restaurant.jpg');
+  img.classList.add('restaurant-img');
+  img.setAttribute('alt', 'restaurant');
+  // create paragraph
+  const p = document.createElement('p');
+  p.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis laboriosam perferendis qui alias dolorem velit blanditiis debitis vero, nemo voluptatibus corrupti voluptatem error repellendus iste, provident illum? Similique, omnis est.';
+  // add them inside wraper div
+  wrapper.appendChild(h1);
+  wrapper.appendChild(img);
+  wrapper.appendChild(p);
+
+  return wrapper;
+};
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import './main.css';
 import './index.html';
+import generateHomepage from './homepage';
 
+const testDiv = generateHomepage();
+document.querySelector('#content').appendChild(testDiv);
